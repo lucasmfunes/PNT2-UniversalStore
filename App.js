4c2c4a2a34a6a0b8ebce6ddc2e9d654d73a00f94
Guido Agustín Compagno <guidocompagno@gmail.com>
2024-02-18 23:42:06 -0300

    packages updates test

--- App.js
+++ App.js
@@ -1,57 +1,64 @@
-import Login from './src/components/login'
-import Detail from './src/components/productDetails'
-import ProductList from './src/components/productList';
-import Cart from './src/components/carrito'
-import OrderPayed from './src/components/orderSuccesful';
-import React, {useContext, useEffect, useState} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import GlobalContext,{defaultAuth, cartList}  from './src/components/globalContext'; 
-import Storage from './services/asyncStorage'
+import Login from "./src/components/login";
+import Detail from "./src/components/productDetails";
+import ProductList from "./src/components/productList";
+import Cart from "./src/components/carrito";
+import OrderPayed from "./src/components/orderSuccesful";
+import React, { useContext, useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import GlobalContext, {
+  defaultAuth,
+  cartList,
+} from "./src/components/globalContext";
+import Storage from "./services/asyncStorage";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [userAuth, setuserAuth] = useState(defaultAuth)
-  const [cartProductList, setCartProductList] = useState(cartList)
+  const [userAuth, setuserAuth] = useState(defaultAuth);
+  const [cartProductList, setCartProductList] = useState(cartList);
 
   useEffect(() => {
-    Storage.getData('Auth')
-    .then(res => setuserAuth(JSON.parse(res)))
-    .catch(error => console.log(error))
+    Storage.getData("Auth")
+      .then((res) => setuserAuth(JSON.parse(res)))
+      .catch((error) => console.log(error));
 
-    Storage.getData('Cart')
-    .then((res) => {
-      if(res != null && res != undefined){
-        setCartProductList(JSON.parse(res))
-      } else{
-        setCartProductList([])
-      }
-    })
-    .catch(error => console.log(error))
+    Storage.getData("Cart")
+      .then((res) => {
+        if (res != null && res != undefined) {
+          setCartProductList(JSON.parse(res));
+        } else {
+          setCartProductList([]);
+        }
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   return (
-    <GlobalContext.Provider value={
-      {userAuth,
-      setuserAuth,
-      cartProductList,
-      setCartProductList}
-    }>
+    <GlobalContext.Provider
+      value={{ userAuth, setuserAuth, cartProductList, setCartProductList }}
+    >
       <NavigationContainer>
         <Stack.Navigator>
-          {
-            (userAuth) ?
-              <>
-                <Stack.Screen name="Main" component={ProductList} options={{headerShown: false}}/>
-                <Stack.Screen name="Detail" component={Detail} />
-                <Stack.Screen name="Cart" component={Cart} />
-                <Stack.Screen name="Payment" component={OrderPayed}  options={{headerBackVisible: false}}/>
-              </> :
-              <Stack.Screen name="Login" component={Login} /> 
-          }
+          {userAuth ? (
+            <>
+              <Stack.Screen
+                name="Main"
+                component={ProductList}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen name="Detail" component={Detail} />
+              <Stack.Screen name="Cart" component={Cart} />
+              <Stack.Screen
+                name="Payment"
+                component={OrderPayed}
+                options={{ headerBackVisible: false }}
+              />
+            </>
+          ) : (
+            <Stack.Screen name="Login" component={Login} />
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </GlobalContext.Provider>
   );
 }
-
--- src/components/login.js
+++ src/components/login.js
@@ -1,17 +1,32 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {Button, TextInput, View, StyleSheet, Image } from 'react-native';
-import logo from '../../assets/universal-logo.jpg';
-import * as Google from 'expo-auth-session/providers/google';
-import {ANDROID_CLIENT_ID, IOS_CLIENT_ID, EXPO_CLIENT_ID} from "@env"
-import GlobalContext from './globalContext'; 
-import Storage from '../../services/asyncStorage'
+import React, { useContext, useEffect, useState } from "react";
+import { Button, TextInput, View, StyleSheet, Image } from "react-native";
+import logo from "../../assets/universal-logo.jpg";
+import * as Google from "expo-auth-session/providers/google";
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID, EXPO_CLIENT_ID } from "@env";
+import GlobalContext from "./globalContext";
+import Storage from "../../services/asyncStorage";
 
 const Login = () => {
   const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: '733272611796-b9l767bn0rd3gd0gq1l5kav0kt8a637j.apps.googleusercontent.com',
-    iosClientId: '733272611796-1lnaoe7cskm42ihrjl9q2vqfma9gme20.apps.googleusercontent.com',
-    expoClientId: '733272611796-sqgchop4lgq9dnk89ogcplk1qfr2rkkr.apps.googleusercontent.com'
+    androidClientId:
+      "353173646980-dbntp77t6k187qg43nv2b0tljldj6bd3.apps.googleusercontent.com",
+    iosClientId:
+      "353173646980-lv9vtkppgmfpgu19o9g8h56ihlfu0dba.apps.googleusercontent.com",
+    expoClientId:
+      "353173646980-7m3okg2d627p7g44tkn5703u6kc4r27c.apps.googleusercontent.com",
+    /*androidClientId:
+      "733272611796-b9l767bn0rd3gd0gq1l5kav0kt8a637j.apps.googleusercontent.com",
+    iosClientId:
+      "733272611796-1lnaoe7cskm42ihrjl9q2vqfma9gme20.apps.googleusercontent.com",
+    expoClientId:
+      "733272611796-sqgchop4lgq9dnk89ogcplk1qfr2rkkr.apps.googleusercontent.com",*/
   });
+  /*const [request, response, promptAsync] = useAuthRequest({
+    responseType: "token",
+    clientId:
+      "353173646980-odi3eu9tbj1ktq9mebvnrclg3san39pv.apps.googleusercontent.com",
+    scopes: ["openid", "profile", "email"],
+  });*/
 
   // const [request, response, promptAsync] = Google.useAuthRequest({
   //   androidClientId: '733272611796-b9l767bn0rd3gd0gq1l5kav0kt8a637j.apps.googleusercontent.com',
@@ -20,100 +35,107 @@ const Login = () => {
   // });
 
   // Log with google
-  const [accessToken, setAccessToken ] = useState();
-  const{userAuth, setuserAuth} = useContext(GlobalContext)
+  const [accessToken, setAccessToken] = useState();
+  const { userAuth, setuserAuth } = useContext(GlobalContext);
 
   const logWithGoogle = () => {
-    promptAsync({showInRecents: true})
-    .then((value)=>{
-        return fetch("https://www.googleapis.com/userinfo/v2/me" ,{
-        headers: {Authorization: `Bearer ${value.authentication.accessToken}` }}
-    )
-    }
-    ).then( (data) => { 
-      data.json().then(data => {
-        Storage.storeData("Auth", JSON.stringify(data))
-        setuserAuth(data)
+    promptAsync({ showInRecents: true })
+      .then((value) => {
+        return fetch("https://www.googleapis.com/userinfo/v2/me", {
+          headers: {
+            Authorization: `Bearer ${value.authentication.accessToken}`,
+          },
+        });
       })
-    })
-  }
-
+      .then((data) => {
+        data.json().then((data) => {
+          Storage.storeData("Auth", JSON.stringify(data));
+          setuserAuth(data);
+        });
+      });
+  };
 
   //Log with user
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
   const loginWithUser = () => {
-      if(username == null || password == null){
-        console.log("usuario invalido")
-      } else{
-        setuserAuth({name: username})
-      }
-  }
-
+    if (username == null || password == null) {
+      console.log("usuario invalido");
+    } else {
+      setuserAuth({ name: username });
+    }
+  };
 
   useEffect(() => {
-    if(response?.type === "success"){
-      setAccessToken(response.authentication.accessToken)
+    if (response?.type === "success") {
+      setAccessToken(response.authentication.accessToken);
     }
-  }, [response])
+  }, [response]);
 
-    return (
-      <View style={styles.container}>
-         <Image
-            style={styles.logo}
-            source= {logo}
-        />
-        <TextInput
-          value={this.username}
-          onChangeText={(text) => setUsername(text )}
-          placeholder={'Username'}
-          style={styles.input}
-        />
-        <TextInput
-          value={this.password}
-          onChangeText={(password) => setPassword(password)}
-          placeholder={'Password'}
-          secureTextEntry={true}
-          style={styles.input}
-        />
-        <Button
-          title={'Login'}
-          style={styles.input}
-          onPress={() => {loginWithUser()}}
-        />
-        <Button
-          title="Sign in with Google"
-          disabled={!request}
-          onPress={() => {
-           logWithGoogle()
-          }}
-        />
-      </View>
-    );
+  /*useEffect(() => {
+    if (response?.type === "success") {
+      const { authentication } = response;
+      setAccessToken(authentication.accessToken);
+      // Aquí puedes manejar la respuesta de autenticación
+    }
+  }, [response]);*/
 
-}
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <TextInput
+        //value={this.username}
+        onChangeText={(text) => setUsername(text)}
+        placeholder={"Username"}
+        style={styles.input}
+      />
+      <TextInput
+        //value={this.password}
+        onChangeText={(password) => setPassword(password)}
+        placeholder={"Password"}
+        secureTextEntry={true}
+        style={styles.input}
+      />
+      <Button
+        title={"Login"}
+        style={styles.input}
+        onPress={() => {
+          loginWithUser();
+        }}
+      />
+
+      <Button
+        title="Sign in with Google"
+        disabled={!request}
+        onPress={() => {
+          logWithGoogle();
+        }}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
   },
   logo: {
-        width: 200,
-        height: 100,
-        marginBottom: 20
-    },
+    width: 200,
+    height: 100,
+    marginBottom: 20,
+  },
   input: {
     width: 200,
     height: 44,
     padding: 10,
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
     marginBottom: 10,
   },
 });
 
-export default Login
+export default Login;
